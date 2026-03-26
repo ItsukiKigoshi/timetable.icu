67efd21df3e89d1182cbeaed9ce66bb2e88d8092
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-26 05:51:36 -0700

    update auth-client.ts to match workers .env add PUBLIC_ prefix for astro

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.BETTER_AUTH_URL || "http://localhost:4321"
+    baseURL: import.meta.env.PUBLIC_BASE_URL
 });
 
 export const signInWithGoogle = async () => {
