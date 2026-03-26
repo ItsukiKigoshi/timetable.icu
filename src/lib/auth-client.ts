eaf69eab24372ebe7aee9ba635457d587773a0c7
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-26 05:36:03 -0700

    update auth-client.ts to match workers setting

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,15 +1,13 @@
 import { createAuthClient } from "better-auth/react";
+import { env } from "cloudflare:workers";
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.PROD
-        ? "https://timetable-icu.pages.dev"
-        : "http://localhost:4321",
+    baseURL: env.BETTER_AUTH_URL
 });
 
 export const signInWithGoogle = async () => {
     const { data, error } = await authClient.signIn.social({
         provider: "google",
-        // ログイン完了後のリダイレクト先を指定したい場合はここに追加
         callbackURL: "/",
     });
 
