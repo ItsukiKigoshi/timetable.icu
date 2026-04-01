270cf553b42711f0aa9c83112335237e993a968d
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-04-01 14:26:43 -0700

    add i18n support more

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import {defaultLang, languages} from "./src/translation/ui.ts"; // @エイリアスは使えない
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
     adapter: cloudflare(),
+    i18n: {
+        defaultLocale: defaultLang,
+        locales: languages,
+        routing: {
+            prefixDefaultLocale: false,
+        },
+    },
     integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
