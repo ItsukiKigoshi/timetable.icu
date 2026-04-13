65ba6d044e55f63fa98dab6f3a240997da0007d8
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-04-13 02:38:06 -0700

    remove unnecessary break

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,10 @@
 export const limitPerPage = 20; // 検索画面における1ページあたりの件数
 
 export const SWATCHES = [
-  "#15aabf",
+	"#15aabf",
 	"#40c057",
 	"#fab005",
 	"#fa5252",
 	"#be4bdb",
-
 	"#868e96",
 ];
