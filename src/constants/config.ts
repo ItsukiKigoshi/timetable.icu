268bb43d84ae5bc5770a2a7704a8db41845c3704
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-04-13 02:37:46 -0700

    choose colour swatch

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,17 +1,11 @@
 export const limitPerPage = 20; // 検索画面における1ページあたりの件数
 
 export const SWATCHES = [
-	"#868e96",
-	"#fa5252",
-	"#e64980",
-	"#be4bdb",
-	"#7950f2",
-	"#4c6ef5",
-	"#228be6",
-	"#15aabf",
-	"#12b886",
+  "#15aabf",
 	"#40c057",
-	"#82c91e",
 	"#fab005",
-	"#fd7e14",
+	"#fa5252",
+	"#be4bdb",
+
+	"#868e96",
 ];
